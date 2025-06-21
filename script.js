7aef36d0aac25d2e82b45fa12847edf04e31614e
Nathan <nathanbrentlloyd2000@gmail.com>
2025-06-21 06:02:15 +0000

    Changes

--- script.js
+++ script.js
@@ -2,29 +2,22 @@
 
 // I'm not sure why I created a function to declare these variables
 // The variables would've just been initialised in the main game function
-function getScore() {
+const getScore = () => {
     let playerScore = 0;
     let compScore = 0;
     return { playerScore, compScore };
 }
 
-function returnOptions() {
-    // Call .map() on the array
+const returnOptions = () => {
     // The .map() function iterates through array and makes each element lowercase
-    return [
-        "Rock", 
-        "Paper", 
-        "Scissors"
-    ].map((e) => e.toLowerCase());
+    return ["Rock", "Paper", "Scissors"].map((e) => e.toLowerCase());
 }
 
-
-function getComputerChoice() {
+const getComputerChoice = () => {
     const choices = returnOptions();
     const rand = Math.floor(Math.random() * choices.length);
     return choices[rand];
 }
-
 
 const getHumanChoice = () => {
     const pInput = prompt("Rock, Paper or Scissors?").toLowerCase();
@@ -32,17 +25,14 @@ const getHumanChoice = () => {
 
     if (choices.indexOf(pInput) < 0 || !pInput)
         console.log("Trash!!");
-
     return pInput;
 }
 
-
-function checkMove(player, move) {
+const checkMove = (player, move) => {
     return player === move;
 }
 
-
-function gameStart() {
+const gameStart = () => {
     // Retrieve the score variables from the getScore function
     let { playerScore, compScore } = { ...getScore() };
     let wRounds = 5;
@@ -53,10 +43,11 @@ function gameStart() {
             checkMove(player, "scissors") && checkMove(comp, "paper")) {
                 playerScore++;
                 console.log(`You win, ${player} beats ${comp}!!`);
-        } else if (
-            checkMove(comp, "rock") && checkMove(player, "scissors") || 
-            checkMove(comp, "paper") && checkMove(player, "rock") ||
-            checkMove(comp, "scissors") && checkMove(player, "paper")) {
+        } 
+        
+        else if (checkMove(comp, "rock") && checkMove(player, "scissors") || 
+                checkMove(comp, "paper") && checkMove(player, "rock") || 
+                checkMove(comp, "scissors") && checkMove(player, "paper")) {
                 compScore++;
                 console.log(`You lose, ${comp} beats ${player}`);
         }
@@ -68,7 +59,6 @@ function gameStart() {
     while (playerScore < wRounds && compScore < wRounds) {
         let player = getHumanChoice();
         let comp = getComputerChoice();
-
         playRound(player, comp);
     }
     if (playerScore > compScore) console.log("You Win!!");
