ef0bce54327c9ff6e68f11d1c74490b7b8f0b4bd
Nathan <nathanbrentlloyd2000@gmail.com>
2025-07-14 17:22:00 +0000

    Add div for player lose image, add style for div, fix js game logic

--- script.js
+++ script.js
@@ -8,36 +8,85 @@ const getComputerChoice = () => {
     return choices[rand];
 }
 
-const getHumanChoice = () => {
-    const pInput = prompt("Rock, Paper or Scissors?").toLowerCase();
-    const choices = returnOptions();
-    if (choices.indexOf(pInput) < 0 || !pInput) console.log("Trash!!");
-    return pInput;
+const checkMove = (player, move, oPlayer, oMove) => {
+    return player === move && oPlayer === oMove;
 }
 
-const checkMove = (player, move, oPlayer, oMove) => player === move && oPlayer === oMove;
-
 function gameStart() {
-    let [ playerScore, compScore ] = [ 0, 0 ];
+    const rBtn = document.querySelector('#rock');
+    const pBtn = document.querySelector('#paper');
+    const sBtn = document.querySelector('#scissors');
+
+    const cScore = document.querySelector('.computer-score');
+    const pScore = document.querySelector('.player-score');
+    const results = document.querySelector('.display-winner');
+
+    let [ playerScore, compScore ] = [0, 0];
     let wRounds = 5;
 
-    function playRound(player, comp) {
-        if (player === comp) console.log(`Tie: ${player} : ${comp}`); 
-        else if (checkMove(player, "rock", comp, "scissors") || checkMove(player, "paper", comp, "rock") || checkMove(player, "scissors", comp, "paper")) {
+    function playRound(player, computer) {
+        if (playerScore < wRounds && compScore < wRounds) {
+            if (player === computer) results.textContent = `Tie!!`;
+            else if (
+                checkMove(player, "rock", computer, "scissors") ||
+                checkMove(player, "paper", computer, "rock") ||
+                checkMove(player, "scissors", computer, "paper")) {
                 playerScore++;
-                console.log(`You win, ${player} beats ${comp}!!`);
-        } else if (checkMove(comp, "rock", player, "scissors") || checkMove(comp, "paper", player, "rock") || checkMove(comp, "scissors", player, "paper")) {
+                results.textContent = `You win: ${player} beats ${computer}`;
+            } else {
                 compScore++;
-                console.log(`You lose, ${comp} beats ${player}`);
-        } 
+                results.textContent = `You lose: ${computer} beats ${player}`;
+            }
+            pScore.textContent = `Player: ${playerScore}`;
+            cScore.textContent = `Computer: ${compScore}`;
+            highlightComputerChoice(computer);
+        }
+        if (playerScore === wRounds || compScore === wRounds) {
+            let playerWin = playerScore > compScore && playerScore === wRounds;
+            let compWin = compScore > playerScore && compScore === wRounds;
+            results.textContent = `${playerWin ? 'You Win!!' : compWin ? 'You Lose!!' : ''}`;
+            if (compWin) {
+                const img = document.createElement('img');
+                const parent = document.querySelector('.get-rekt');
+
+                img.src = `./assets/icons8-obscene-gesture-50.png`;
+                img.alt = `Get rekt loser`;
+                img.setAttribute('style', 'width: 150px; height: 150px;');
+                parent.appendChild(img);
+            }
+            setTimeout(() => {
+                endGame(player, document.querySelector('img'));
+            }, 4000);
+        }
     }
 
-    while (playerScore < wRounds && compScore < wRounds) {
-        let [player , comp] = [getHumanChoice(), getComputerChoice()];
-        playRound(player, comp);
+    function endGame(player, node) {
+        if (playerScore === wRounds || compScore === wRounds) {
+            playerScore = 0;
+            compScore = 0;
+            if (node !== null) {
+                document.querySelector('.get-rekt').removeChild(node);
+            }
+        }
     }
-    if (playerScore > compScore) console.log("You Win!!");
-    else console.log("You Lose!!");
+
+    rBtn.addEventListener('click', () => {
+        playRound("rock", getComputerChoice());
+    });
+    pBtn.addEventListener('click', (event) => {
+        playRound("paper", getComputerChoice());
+    });
+    sBtn.addEventListener('click', (event) => {
+        playRound("scissors", getComputerChoice());
+    });
+}
+
+function highlightComputerChoice(comp) {
+    const cChoice = document.querySelector(`#${comp}`);
+    cChoice.classList.add('c-highlight');
+    setTimeout(() => {
+        cChoice.classList.remove('c-highlight');
+    }, 900);
 }
 
 gameStart();
